6c1ed2ef86a12dd26eb68835b2ebbf6923b51569
KiritoDv <kiritodev01@gmail.com>
2025-03-15 14:04:12 -0600

    Fully fixed compilation fr

--- include/sf64math.h
+++ include/sf64math.h
@@ -13,6 +13,7 @@ typedef struct Vec3f {
     /* 0x4 */ f32 y;
     /* 0x8 */ f32 z;
 #ifdef __cplusplus
+    Vec3f() : x(0), y(0), z(0) {} // sol:not_global
     Vec3f(f32 x, f32 y, f32 z) : x(x), y(y), z(z) {} // sol:not_global
     Vec3f* asRef() { return this; } // sol:not_global
     f32* xRef() { return &x; } // sol:not_global
@@ -27,6 +28,7 @@ typedef struct Vec3s {
     /* 0x2 */ s16 y;
     /* 0x4 */ s16 z;
 #ifdef __cplusplus
+    Vec3s() : x(0), y(0), z(0) {} // sol:not_global
     Vec3s(s16 x, s16 y, s16 z) : x(x), y(y), z(z) {} // sol:not_global
     Vec3s* asRef() { return this; } // sol:not_global
     s16* xRef() { return &x; } // sol:not_global
