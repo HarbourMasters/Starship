c103d0974ad82b80d0c475fa47b3a761fae2897e
KiritoDv <kiritodev01@gmail.com>
2025-03-14 19:08:09 -0600

    Added error handling, merged script, added errors and implemented MessageFactory on xml

--- src/port/resource/importers/MessageFactory.h
+++ src/port/resource/importers/MessageFactory.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Resource.h"
+#include "ResourceFactoryXML.h"
 #include "ResourceFactoryBinary.h"
 
 namespace SF64 {
@@ -8,5 +9,10 @@ class ResourceFactoryBinaryMessageV0 : public Ship::ResourceFactoryBinary {
   public:
     std::shared_ptr<Ship::IResource> ReadResource(std::shared_ptr<Ship::File> file,
                                                   std::shared_ptr<Ship::ResourceInitData> initData) override;
+};
+
+class ResourceFactoryXMLMessageV0 : public Ship::ResourceFactoryXML {
+  public:
+    std::shared_ptr<Ship::IResource> ReadResource(std::shared_ptr<Ship::File> file) override;
 };
 }; // namespace LUS
